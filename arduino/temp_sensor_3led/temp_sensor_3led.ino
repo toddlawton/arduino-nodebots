5fe55ee541a1e09536a9554855f87ea5f1c47113
Todd Beauchamp <toddbee@gmail.com>
2014-08-08 13:59:36 -0400

    Temp sensor sketch

---
+++ arduino/temp_sensor_3led/temp_sensor_3led.ino
@@ -0,0 +1,54 @@
+const int sensorPin = A0;
+float baselineTemp = 24.0;
+
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+
+  Serial.begin(9600); // Open a serial port
+
+  for (int pinNumber = 2; pinNumber < 5; pinNumber ++) {
+    digitalWrite(pinNumber, HIGH);
+  }
+
+}
+
+void loop() {
+  int sensorVal = analogRead(sensorPin);
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorVal);
+
+  // Convert the ADC reading to voltage
+  float voltage = (sensorVal/1024.0) * 5.0;
+
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+
+  Serial.print(", Degrees C: ");
+  float temperature = (voltage - .5) * 100;
+  Serial.println(temperature);
+
+  if (temperature < baselineTemp) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  } 
+  else if (temperature >= baselineTemp+1 && temperature < baselineTemp+2) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  } 
+  else if (temperature >= baselineTemp+2 && temperature < baselineTemp +3) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  } 
+  else if (temperature >= baselineTemp+3) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+  delay(1);
+}
+
